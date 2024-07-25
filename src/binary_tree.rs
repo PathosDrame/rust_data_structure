cd5fe90b2e6ebaf632dd1c98a8cce0823a60654d
PathosDrame <862915265@qq.com>
2024-07-25 18:00:48 +0800

    feature: support binary tree

---
+++ src/binary_tree.rs
@@ -0,0 +1,311 @@
+use std::{cell::RefCell, rc::Rc};
+
+#[allow(unused)]
+#[derive(Debug, PartialEq, Eq)]
+struct Node {
+    val: i32,
+    left: Option<Rc<RefCell<Node>>>,
+    right: Option<Rc<RefCell<Node>>>,
+}
+
+#[allow(unused)]
+#[derive(Debug, PartialEq, Eq)]
+struct Tree {
+    root: Option<Rc<RefCell<Node>>>,
+    cnt: i32,
+}
+
+#[allow(unused)]
+impl Tree {
+    fn new(node: Option<Rc<RefCell<Node>>>) -> Self {
+        if let Some(root) = node {
+            Self {
+                root: Some(root),
+                cnt: 1,
+            }
+        } else {
+            Self { root: None, cnt: 0 }
+        }
+    }
+
+    fn insert(
+        &mut self,
+        parent: Option<Rc<RefCell<Node>>>,
+        left: Option<Rc<RefCell<Node>>>,
+        right: Option<Rc<RefCell<Node>>>,
+    ) {
+        let root = self.root.as_mut();
+        if let (Some(_), Some(parent)) = (root, parent) {
+            parent.borrow_mut().left.clone_from(&left);
+            parent.borrow_mut().right.clone_from(&right);
+            if left.is_some() {
+                self.cnt += 1;
+            }
+            if right.is_some() {
+                self.cnt += 1;
+            }
+        }
+    }
+
+    fn pre_order(node: Option<Rc<RefCell<Node>>>, vec: &mut Vec<i32>) {
+        if let Some(node) = node {
+            vec.push(node.borrow().val);
+            Self::pre_order(node.borrow().left.clone(), vec);
+            Self::pre_order(node.borrow().right.clone(), vec);
+        }
+    }
+
+    fn pre_order_recur(&self) -> Vec<i32> {
+        let mut vec = Vec::new();
+        Self::pre_order(self.root.clone(), &mut vec);
+        vec
+    }
+
+    fn in_order(node: Option<Rc<RefCell<Node>>>, vec: &mut Vec<i32>) {
+        if let Some(node) = node {
+            Self::in_order(node.borrow().left.clone(), vec);
+            vec.push(node.borrow().val);
+            Self::in_order(node.borrow().right.clone(), vec);
+        }
+    }
+
+    fn in_order_recur(&self) -> Vec<i32> {
+        let mut vec = Vec::new();
+        Self::in_order(self.root.clone(), &mut vec);
+        vec
+    }
+
+    fn pos_order(node: Option<Rc<RefCell<Node>>>, vec: &mut Vec<i32>) {
+        if let Some(node) = node {
+            Self::pos_order(node.borrow().left.clone(), vec);
+            Self::pos_order(node.borrow().right.clone(), vec);
+            vec.push(node.borrow().val);
+        }
+    }
+
+    fn pos_order_recur(&self) -> Vec<i32> {
+        let mut vec = Vec::new();
+        Self::pos_order(self.root.clone(), &mut vec);
+        vec
+    }
+}
+
+#[allow(unused)]
+impl Node {
+    fn new(val: i32) -> Self {
+        Self {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn tree_insert_should_work() {
+        let node1 = Rc::new(RefCell::new(Node::new(1)));
+        let node2 = Rc::new(RefCell::new(Node::new(2)));
+        let node3 = Rc::new(RefCell::new(Node::new(3)));
+        let node4 = Rc::new(RefCell::new(Node::new(4)));
+        let node5 = Rc::new(RefCell::new(Node::new(5)));
+        let node6 = Rc::new(RefCell::new(Node::new(6)));
+
+        let mut tree = Tree::new(Some(node1.clone()));
+        assert_eq!(
+            Tree {
+                root: Some(Rc::new(RefCell::new(Node {
+                    val: 1,
+                    left: None,
+                    right: None,
+                }))),
+                cnt: 1
+            },
+            tree
+        );
+
+        tree.insert(
+            Some(node1.clone()),
+            Some(node2.clone()),
+            Some(node3.clone()),
+        );
+
+        assert_eq!(
+            Tree {
+                root: Some(Rc::new(RefCell::new(Node {
+                    val: 1,
+                    left: Some(Rc::new(RefCell::new(Node {
+                        val: 2,
+                        left: None,
+                        right: None,
+                    }))),
+                    right: Some(Rc::new(RefCell::new(Node {
+                        val: 3,
+                        left: None,
+                        right: None,
+                    })))
+                }))),
+                cnt: 3
+            },
+            tree
+        );
+
+        tree.insert(
+            Some(node2.clone()),
+            Some(node4.clone()),
+            Some(node5.clone()),
+        );
+
+        assert_eq!(
+            Tree {
+                root: Some(Rc::new(RefCell::new(Node {
+                    val: 1,
+                    left: Some(Rc::new(RefCell::new(Node {
+                        val: 2,
+                        left: Some(Rc::new(RefCell::new(Node {
+                            val: 4,
+                            left: None,
+                            right: None,
+                        }))),
+                        right: Some(Rc::new(RefCell::new(Node {
+                            val: 5,
+                            left: None,
+                            right: None,
+                        }))),
+                    }))),
+                    right: Some(Rc::new(RefCell::new(Node {
+                        val: 3,
+                        left: None,
+                        right: None,
+                    })))
+                }))),
+                cnt: 5
+            },
+            tree
+        );
+
+        tree.insert(Some(node3.clone()), Some(node6.clone()), None);
+
+        assert_eq!(
+            Tree {
+                root: Some(Rc::new(RefCell::new(Node {
+                    val: 1,
+                    left: Some(Rc::new(RefCell::new(Node {
+                        val: 2,
+                        left: Some(Rc::new(RefCell::new(Node {
+                            val: 4,
+                            left: None,
+                            right: None,
+                        }))),
+                        right: Some(Rc::new(RefCell::new(Node {
+                            val: 5,
+                            left: None,
+                            right: None,
+                        }))),
+                    }))),
+                    right: Some(Rc::new(RefCell::new(Node {
+                        val: 3,
+                        left: Some(Rc::new(RefCell::new(Node {
+                            val: 6,
+                            left: None,
+                            right: None,
+                        }))),
+                        right: None,
+                    })))
+                }))),
+                cnt: 6
+            },
+            tree
+        );
+    }
+
+    #[test]
+    fn pre_order_recur_should_work() {
+        let node1 = Rc::new(RefCell::new(Node::new(1)));
+        let node2 = Rc::new(RefCell::new(Node::new(2)));
+        let node3 = Rc::new(RefCell::new(Node::new(3)));
+        let node4 = Rc::new(RefCell::new(Node::new(4)));
+        let node5 = Rc::new(RefCell::new(Node::new(5)));
+        let node6 = Rc::new(RefCell::new(Node::new(6)));
+
+        let mut tree = Tree::new(Some(node1.clone()));
+
+        tree.insert(
+            Some(node1.clone()),
+            Some(node2.clone()),
+            Some(node3.clone()),
+        );
+
+        tree.insert(
+            Some(node2.clone()),
+            Some(node4.clone()),
+            Some(node5.clone()),
+        );
+
+        tree.insert(Some(node3.clone()), Some(node6.clone()), None);
+
+        let vec = tree.pre_order_recur();
+        assert_eq!(vec![1, 2, 4, 5, 3, 6], vec);
+    }
+
+    #[test]
+    fn in_order_recur_should_work() {
+        let node1 = Rc::new(RefCell::new(Node::new(1)));
+        let node2 = Rc::new(RefCell::new(Node::new(2)));
+        let node3 = Rc::new(RefCell::new(Node::new(3)));
+        let node4 = Rc::new(RefCell::new(Node::new(4)));
+        let node5 = Rc::new(RefCell::new(Node::new(5)));
+        let node6 = Rc::new(RefCell::new(Node::new(6)));
+
+        let mut tree = Tree::new(Some(node1.clone()));
+
+        tree.insert(
+            Some(node1.clone()),
+            Some(node2.clone()),
+            Some(node3.clone()),
+        );
+
+        tree.insert(
+            Some(node2.clone()),
+            Some(node4.clone()),
+            Some(node5.clone()),
+        );
+
+        tree.insert(Some(node3.clone()), Some(node6.clone()), None);
+
+        let vec = tree.in_order_recur();
+        assert_eq!(vec![4, 2, 5, 1, 6, 3], vec);
+    }
+
+    #[test]
+    fn pos_order_recur_should_work() {
+        let node1 = Rc::new(RefCell::new(Node::new(1)));
+        let node2 = Rc::new(RefCell::new(Node::new(2)));
+        let node3 = Rc::new(RefCell::new(Node::new(3)));
+        let node4 = Rc::new(RefCell::new(Node::new(4)));
+        let node5 = Rc::new(RefCell::new(Node::new(5)));
+        let node6 = Rc::new(RefCell::new(Node::new(6)));
+
+        let mut tree = Tree::new(Some(node1.clone()));
+
+        tree.insert(
+            Some(node1.clone()),
+            Some(node2.clone()),
+            Some(node3.clone()),
+        );
+
+        tree.insert(
+            Some(node2.clone()),
+            Some(node4.clone()),
+            Some(node5.clone()),
+        );
+
+        tree.insert(Some(node3.clone()), Some(node6.clone()), None);
+
+        let vec = tree.pos_order_recur();
+        assert_eq!(vec![4, 5, 2, 6, 3, 1], vec);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod array_queue;
 mod array_stack;
+mod binary_tree;
 mod link_list;
 mod link_queue;
 mod link_stack;
